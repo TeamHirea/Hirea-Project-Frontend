04e7cc6c01d2c7df3d1e0b73cfd67db5f7c7e486
fadildr <drfadildr12@gmail.com>
2022-10-18 23:30:40 +0700

    feat: add signup jobseeker

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,15 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import PublicRoute from "./utils/routes/PublicRoute.js";
 import PrivateRoute from "./utils/routes/PrivateRoute";
-
+import SignupJobSeeker from "./pages/SignupJobSeeker";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route element={<PublicRoute />}></Route>
+        <Route path="/SignupJobSeeker" element={<SignupJobSeeker />} />
 
         <Route element={<PrivateRoute />}></Route>
-
         <Route path="/" element={<Home />} />
       </Routes>
     </BrowserRouter>
