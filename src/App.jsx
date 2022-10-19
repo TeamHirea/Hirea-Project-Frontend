0f3a6cbee886155b5818ba9fe4fdfbdb534a240d
Bill Hikmah <billhikmah1@yahoo.co.id>
2022-10-19 23:55:45 +0700

    Feat: add public, jobseeker, and recruiter route.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
-import PublicRoute from "./utils/routes/PublicRoute.js";
-import PrivateRoute from "./utils/routes/PrivateRoute";
+import PublicRoute from "./utils/routes/RecruiterRoute.js";
+import RecruiterRoute from "./utils/routes/RecruiterRoute";
+import JobseekerRoute from "./utils/routes/JobseekerRoute";
 import SignupJobSeeker from "./pages/SignupJobSeeker";
 import Landing from "./pages/Landing";
 
@@ -13,11 +14,11 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route element={<PublicRoute />}></Route>
-        <Route element={<PrivateRoute />}></Route>
+        <Route element={<RecruiterRoute />}></Route>
+        <Route element={<JobseekerRoute />}></Route>
+
         <Route path="/home" element={<Home />} />
         <Route path="/" element={<Landing />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/" element={<Home />} />
         <Route path="/SignupJobSeeker" element={<SignupJobSeeker />} />
         <Route path="/SignupRecruiter" element={<SignupRecruiter />} />
         <Route path="/hire" element={<Hire />} />
