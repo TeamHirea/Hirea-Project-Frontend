365e0719e446d5eb09dd3a86d809dd402e8c9749
Bill Hikmah <billhikmah1@yahoo.co.id>
2022-10-19 16:54:29 +0700

    Update route

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,15 @@ import Home from "./pages/Home";
 import PublicRoute from "./utils/routes/PublicRoute.js";
 import PrivateRoute from "./utils/routes/PrivateRoute";
 import SignupJobSeeker from "./pages/SignupJobSeeker";
+import Landing from "./pages/Landing";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route element={<PublicRoute />}></Route>
         <Route element={<PrivateRoute />}></Route>
-        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/" element={<Landing />} />
         <Route path="/SignupJobSeeker" element={<SignupJobSeeker />} />
       </Routes>
     </BrowserRouter>
