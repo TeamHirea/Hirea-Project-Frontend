648c1f557c2c36c035ba7eca9d7578417174a80d
Bill Hikmah <billhikmah1@yahoo.co.id>
2022-10-23 02:25:20 +0700

    Update reducer

--- src/redux/reducer/user.js
+++ src/redux/reducer/user.js
@@ -1,12 +1,51 @@
 const initialState = {
-  data: [],
+  data: {},
+  isLoading: false,
+  isError: false,
+  errorMessage: "",
 };
 
 const userReducer = (state = initialState, action) => {
   switch (action.type) {
-    case "SIGNIN_PENDING":
+    case "GET_PROFILE_JOBSEEKER_PENDING":
       return {
         ...state,
+        isLoading: true,
+      };
+    case "GET_PROFILE_JOBSEEKER_FULFILLED":
+      return {
+        ...state,
+        data: action.payload.data.data[0],
+        isLoading: false,
+        isError: false,
+        errorMessage: "",
+      };
+    case "GET_PROFILE_JOBSEEKER_REJECTED":
+      return {
+        ...state,
+        isLoading: false,
+        isError: true,
+        errorMessage: action.payload.response.data.message,
+        data: {},
+      };
+    case "UPDATE_PROFILE_JOBSEEKER_PENDING":
+      return {
+        ...state,
+        isLoading: true,
+      };
+    case "UPDATE_PROFILE_JOBSEEKER_FULFILLED":
+      return {
+        ...state,
+        isLoading: false,
+        isError: false,
+        errorMessage: "",
+      };
+    case "UPDATE_PROFILE_JOBSEEKER_REJECTED":
+      return {
+        ...state,
+        isLoading: false,
+        isError: true,
+        errorMessage: action.payload.data.message,
       };
     default: {
       return state;
