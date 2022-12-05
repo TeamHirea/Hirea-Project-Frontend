d5054d255c9158708bea7fd8a0015c1f9f8ee91a
billhikmah <billhikmah1@yahoo.co.id>
2022-12-06 01:22:22 +0700

    Fix: Bug

--- src/pages/ResetPassword/index.jsx
+++ src/pages/ResetPassword/index.jsx
@@ -29,102 +29,47 @@ export default function ResetPassword() {
       <div className="container py-0 ">
         <div className="container-auth rounded-1">
           <div className="row-auth row">
-            <div className="col-md-6 col-left">
-              <img
-                src={logo}
-                style={{ width: "15%", cursor: "pointer" }}
-                onClick={() => {
-                  navigate("/");
-                }}
-                className="logo-auth"
-              />
-              <h4 className="desc-left">
-                Temukan developer berbakat & terbaik di berbagai bidang keahlian
-              </h4>
-              <div className="col-lg-6 signinRecruiter--page--first__container">
-                <img src={logo} style={{ width: "15%" }} className="" alt="" />
-                <div className="signinRecruiter--content__container">
-                  <p className="signinRecruiter--content__style px-4">
-                    Temukan developer berbakat & terbaik di berbagai bidang
-                    keahlian
-                  </p>
-                </div>
+            <div className="col-lg-6 signinRecruiter--page--first__container">
+              <img src={logo} style={{ width: "15%" }} className="" alt="" />
+              <div className="signinRecruiter--content__container">
+                <p className="signinRecruiter--content__style px-4">
+                  Temukan developer berbakat & terbaik di berbagai bidang
+                  keahlian
+                </p>
               </div>
-              <div className="col-md-6 col-right bg-white d-flex align-items-center">
-                <div className="auth-right ">
-                  <p className="title-auth"> Halo, Pewpeople</p>
-                  <p className="desc-auth">
-                    Enter your user accounts verified email address and we will{" "}
-                    <br />
-                    send you a password reset link
-                  </p>
-                  <p className="label mt-3">Email</p>
-                  <input
-                    type="text"
-                    name="email"
-                    className="form-control"
-                    placeholder="Input Your Email"
-                    onChange={handleChangeForm}
-                  />
+            </div>
+            <div className="col-md-6 col-right bg-white d-flex align-items-center">
+              <div className="auth-right ">
+                <p className="title-auth"> Halo, Pewpeople</p>
+                <p className="desc-auth">
+                  Enter your user accounts verified email address and we will{" "}
+                  <br />
+                  send you a password reset link
+                </p>
+                <p className="label mt-3">Email</p>
+                <input
+                  type="text"
+                  name="email"
+                  className="form-control"
+                  placeholder="Input Your Email"
+                  onChange={handleChangeForm}
+                />
 
-                  <button
-                    type="button"
-                    className="btn w-100 btn-auth btn-warning text-white mt-3"
-                    onClick={handleSubmit}
-                  >
-                    {loading ? (
-                      <div
-                        className="spinner-border text-light"
-                        role="status"
-                      ></div>
-                    ) : (
-                      "Send to your email"
-                    )}
-                  </button>
-                </div>{" "}
-              </div>
-              <div className="col-lg-6 signinRecruiter--page--first__container">
-                <img src={logo} style={{ width: "15%" }} className="" alt="" />
-                <div className="signinRecruiter--content__container">
-                  <p className="signinRecruiter--content__style px-4">
-                    Temukan developer berbakat & terbaik di berbagai bidang
-                    keahlian
-                  </p>
-                </div>
-              </div>
-              <div className="col-md-6 col-right bg-white d-flex align-items-center">
-                <div className="auth-right ">
-                  <p className="title-auth"> Halo, Pewpeople</p>
-                  <p className="desc-auth">
-                    Enter your user accounts verified email address and we will{" "}
-                    <br />
-                    send you a password reset link
-                  </p>
-                  <p className="label mt-3">Email</p>
-                  <input
-                    type="text"
-                    name="email"
-                    className="form-control"
-                    placeholder="Input Your Email"
-                    onChange={handleChangeForm}
-                  />
-
-                  <button
-                    type="button"
-                    className="btn w-100 btn-auth btn-warning text-white mt-3"
-                    onClick={handleSubmit}
-                  >
-                    {loading ? (
-                      <div
-                        className="spinner-border text-light"
-                        role="status"
-                      ></div>
-                    ) : (
-                      "Send to your email"
-                    )}
-                  </button>
-                </div>{" "}
-              </div>
+                <button
+                  type="button"
+                  className="btn w-100 btn-auth btn-warning text-white mt-3"
+                  onClick={handleSubmit}
+                >
+                  {loading ? (
+                    <div
+                      className="spinner-border text-light"
+                      role="status"
+                    ></div>
+                  ) : (
+                    "Send to your email"
+                  )}
+                </button>
+              </div>{" "}
             </div>
           </div>
         </div>
