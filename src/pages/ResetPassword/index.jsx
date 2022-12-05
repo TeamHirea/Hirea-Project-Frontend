3cb4d349059d8ca0cf4ec9d94606a0c1cb83795c
setyawandwiki <setyawandwiki1@gmail.com>
2022-12-05 19:53:50 +0700

    styling reset password, dan forgot password

--- src/pages/ResetPassword/index.jsx
+++ src/pages/ResetPassword/index.jsx
@@ -7,13 +7,16 @@ import axios from "../../utils/axios";
 export default function ResetPassword() {
   const navigate = useNavigate();
   const [form, setForm] = useState({});
+  const [loading, setLoading] = useState(false);
   const handleChangeForm = (e) => {
     setForm({ ...form, [e.target.name]: e.target.value });
   };
 
   const handleSubmit = async () => {
+    setLoading(true);
     try {
       const result = await axios.post("/api/auth/forgotPassword", form);
+      setLoading(false);
       alert(result.data.message);
       navigate("/reset/send/success");
     } catch (error) {
@@ -26,13 +29,16 @@ export default function ResetPassword() {
       <div className="container py-0 ">
         <div className="container-auth rounded-1">
           <div className="row-auth row">
-            <div className="col-md-6 col-left">
-              <img src={logo} alt="" className="logo-auth" />
-              <h4 className="desc-left">
-                Temukan developer berbakat & terbaik di berbagai bidang keahlian
-              </h4>
+            <div className="col-lg-6 signinRecruiter--page--first__container">
+              <img src={logo} style={{ width: "15%" }} className="" alt="" />
+              <div className="signinRecruiter--content__container">
+                <p className="signinRecruiter--content__style px-4">
+                  Temukan developer berbakat & terbaik di berbagai bidang
+                  keahlian
+                </p>
+              </div>
             </div>
-            <div className="col-md-6 col-right bg-white">
+            <div className="col-md-6 col-right bg-white d-flex align-items-center">
               <div className="auth-right ">
                 <p className="title-auth"> Halo, Pewpeople</p>
                 <p className="desc-auth">
@@ -44,6 +50,7 @@ export default function ResetPassword() {
                 <input
                   type="text"
                   name="email"
+                  className="form-control"
                   placeholder="Input Your Email"
                   onChange={handleChangeForm}
                 />
@@ -53,7 +60,14 @@ export default function ResetPassword() {
                   className="btn w-100 btn-auth btn-warning text-white mt-3"
                   onClick={handleSubmit}
                 >
-                  Send to your email
+                  {loading ? (
+                    <div
+                      className="spinner-border text-light"
+                      role="status"
+                    ></div>
+                  ) : (
+                    "Send to your email"
+                  )}
                 </button>
               </div>{" "}
             </div>
